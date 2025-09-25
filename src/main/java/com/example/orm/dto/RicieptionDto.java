c45bbfd9e571484cdc3fc38708cfe86f30529cab
Akilach01 <akilapeiris92@gmail.com>
2025-09-25 14:14:32 +0530

    fixing dto's

--- src/main/java/com/example/orm/dto/RicieptionDto.java
+++ src/main/java/com/example/orm/dto/RicieptionDto.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
 public class RicieptionDto {
+
     private String name;
-    private String email;
     private String password;
     private int contact;
+    private String email;
+
 }
