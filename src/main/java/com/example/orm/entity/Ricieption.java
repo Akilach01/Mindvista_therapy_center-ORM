b018f947f159609792c0b000a86911192f077444
Akilach01 <akilapeiris92@gmail.com>
2025-09-25 17:02:21 +0530

    fixing  reception entity

---
+++ src/main/java/com/example/orm/entity/Ricieption.java
@@ -0,0 +1,32 @@
+package com.example.orm.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "ricieption")
+public class Ricieption {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    private int contact;
+
+    public Ricieption(String name, String password, String email, int contact) {
+       this.name = name;
+       this.password = password;
+       this.email = email;
+       this.contact = contact;
+    }
+
+}
